edfa86e1cb12eb4edb22677dbff33fb50969cbb8
Raphael Reyna <raphaelreyna@protonmail.com>
2019-03-24 13:09:42 -0700

    Add flag for number of iterations.

--- src/cliifs.py
+++ src/cliifs.py
@@ -19,12 +19,21 @@ class cliifs:
         self.window.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename", help="File that you want to render.")
+    parser = argparse.ArgumentParser(description = "Render fractals in your terminal.")
+    parser.add_argument("filename",
+                        help="File that you want to render.")
+    parser.add_argument("--iterations",
+                        "-i",
+                        help="Number of iterations to use.",
+                        default=1000,
+                        action="store",
+                        nargs="?",
+                        type=int)
     args = parser.parse_args()
     filename = args.filename
+    iterations = args.iterations
     c = cliifs(filename)
-    c.generatePoints(1000)
+    c.generatePoints(iterations)
     c.showPoints()
     c.close()
 
