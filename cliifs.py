293efc8ea83028fa4f01cb6bfc436289fcad50f3
Raphael Reyna <raphaelreyna@protonmail.com>
2019-03-24 11:00:57 -0700

    Seperate model, view and controller.

--- cliifs.py
+++ cliifs.py
@@ -1,28 +1,27 @@
-import curses
-import random
-import time
-import math
-import IFSLoader
+import IFS
+import PlotWindow
 import sys
 
-def changeCoordsToCurses(x, y, maxX, maxY):
-    return (maxY*(1-y), maxX*x)
+class cliifs:
+    def __init__(self, filename):
+        self.ifs = IFS.loadIFSFromFile(filename)
+        self.window = None
+
+    def generatePoints(self, numberOfIterations):
+        self.ifs.update(numberOfIterations)
+
+    def showPoints(self):
+        self.window = PlotWindow.PlotWindow()
+        self.window.updatePoints(self.ifs.points)
+        self.window.waitForKeyPress()
+
+    def close(self):
+        self.window.close()
 
 if __name__ == "__main__":
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(1)
     filename = sys.argv[1]
-    funcs = IFSLoader.loadFuncsFromFile(filename)
-    point = 0
-    maxY, maxX = stdscr.getmaxyx()
-    for i in range(6000):
-        func = random.choice(funcs)
-        point = func(point)
-        y, x = changeCoordsToCurses(point, 0.5, maxX-1, maxY-1)
-        stdscr.addstr(int(y), int(x), "*")
-        stdscr.refresh()
-    stdscr.getch()
-    curses.endwin()
+    c = cliifs(filename)
+    c.generatePoints(300)
+    c.showPoints()
+    c.close()
 
